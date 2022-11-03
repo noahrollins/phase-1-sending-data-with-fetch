c8331d31f623b15bc8dd23162603255a47ce1220
Noah Rollins <noahjrollins@gmail.com>
2022-11-03 12:10:22 -0500

    commit tree d2f3b81ee994aa7636abdf90d7d51f86cd456e13

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    const submitDataObj = {
+        name: name,
+        email: email
+    }
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify(submitDataObj)
+    })
+    .then(res => res.json())
+    .then((data) => {
+        const p = document.createElement('p');
+        p.textContent = data.id
+        document.body.appendChild(p)
+    })
+    .catch(function (error){
+        const er = document.createElement('p')
+        er.textContent = error.message
+        document.body.appendChild(er)
+    })
+}
+
+submitData('john','jd123@gmail.com')
